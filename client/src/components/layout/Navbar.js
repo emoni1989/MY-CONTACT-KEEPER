ba6101a77422ced72cf183122f3e80e89737c851
Emmanuel matthew <emoni1988@gmail.com>
2021-08-19 04:34:25 +0100

    My contact keeper

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+
+const Navbar = ({ title, icon }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Navbar
